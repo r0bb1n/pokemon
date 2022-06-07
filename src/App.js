9b31cacdaad7774a73ceed86cf0329d28c8879aa
r0bb1n <kjellgrenrobin@gmail.com>
2022-06-07 16:20:07 +0200

    merging

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import Arena from "./components/Arena";
+import { useState } from "react";
+import UserData from "./Data/User";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Account from './Account';
+import Start from './Start';
+import Home from "./Home";
+import Choose from './Choose';
+import Rival from './Rival';
 
 
+function App() {
 
-const App = () => {
+  const [feedback, setFeedback] = useState(UserData);
 
-   
+console.log(feedback);
+
   return (
-    <Arena />
-  );
-}
 
-export default App;
+    <BrowserRouter>
+    <div className="container">
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Account" element={<Account />} />
+    <Route path="/Start" element={<Start />} />
+    </Routes>
+    </div>
+    <div>
+    <Routes>
+    <Route path="/Choose" element={<Choose />} />
+    <Route path="/Rival" element={<Rival />} />
+    </Routes>
+    </div>
+    </BrowserRouter>
+  );
+  }
+
+
+
+
+  export default App;
